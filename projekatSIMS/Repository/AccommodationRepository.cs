62b73fa5cea72e094c5884600d10566baf7c9e31
dunjatrikic <dunja.trikic8@gmail.com>
2023-03-13 21:26:10 +0100

    Dodavanje repozitorijuma za smestaj

---
+++ projekatSIMS/Repository/AccommodationRepository.cs
@@ -0,0 +1,14 @@
+﻿using projekatSIMS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekatSIMS.Repository
+{
+    internal class AccommodationRepository : Repository<Accommodation>
+    {
+
+    }
+}
