8c10c55d91cfe44169ad1dea52caa67dd050f87e
dunjatrikic <dunja.trikic8@gmail.com>
2023-03-13 12:05:47 +0100

    accommodation type

--- projekatSIMS/Model/AccommodationType.cs
+++ projekatSIMS/Model/AccommodationType.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace projekatSIMS.Model
 {
-     public enum AccommondationType
+     public enum AccommodationType
         {
             APARTMENT, HOUSE, COTTAGE
         }
