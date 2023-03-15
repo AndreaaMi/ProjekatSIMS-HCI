49e96469cc5ef1b38e4bd3eb7011187cb16122c8
AndreaaMi <126777292+AndreaaMi@users.noreply.github.com>
2023-03-15 22:45:34 +0100

    AccommodationDisplay.xaml

--- projekatSIMS/UI/Dialogs/View/AccommodationSearchView.xaml.cs
+++ projekatSIMS/UI/Dialogs/View/AccommodationSearchView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using projekatSIMS.UI.Dialogs.ViewModel;
+﻿using projekatSIMS.Model;
+using projekatSIMS.Service;
+using projekatSIMS.UI.Dialogs.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,83 @@ namespace projekatSIMS.UI.Dialogs.View
         {
             DataContext = new AccommodationSearchViewModel();
             InitializeComponent();
+            AccommodationService accommodationService = new AccommodationService();
+            foreach (Accommodation entity in accommodationService.GetAll())
+            {
+                TypesComboBox.Items.Add(entity.Type);
+                CityComboBox.Items.Add(entity.Location.City);
+
+            }
+
+            RemoveDuplicateItemsFromComboBox(TypesComboBox);
+            RemoveDuplicateItemsFromComboBox(CityComboBox);
+        }
+
+        private void RemoveDuplicateItemsFromComboBox(ComboBox comboBox)
+        {
+            for (int i = 0; i < comboBox.Items.Count; i++)
+            {
+                for (int j = i + 1; j < comboBox.Items.Count; j++)
+                {
+                    if (comboBox.Items[i].Equals(comboBox.Items[j]))
+                    {
+                        comboBox.Items.RemoveAt(j);
+                        j--;
+                    }
+                }
+            }
+        }
+
+        private void DisplayAccommodations_Click(object sender, RoutedEventArgs e)
+        {
+
+            MyListBox.Items.Clear();
+
+            AccommodationService accommodationService = new AccommodationService();
+
+            foreach (Accommodation entity in accommodationService.GetAll())
+            {
+                MyListBox.Items.Add(entity.Id + " " + entity.Name + " " + entity.Location.City + " " + entity.Location.Country + " " + entity.GuestLimit + " " + entity.MinimalStay + " " + entity.CancelationLimit);
+            }
+        }
+
+        private void TypesComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MyListBox.Items.Clear();
+            string selectedType = TypesComboBox.SelectedItem.ToString();
+            AccommodationService accommodationService = new AccommodationService();
+            foreach (Accommodation entity in accommodationService.GetAll())
+            {
+                if (entity.Type.ToString() == selectedType)
+                {
+                    MyListBox.Items.Add(entity.Id + " " + entity.Name + " " + entity.Location.City + " " + entity.Location.Country + " " + entity.GuestLimit + " " + entity.MinimalStay + " " + entity.CancelationLimit);
+                }
+
+            }
+        }
+
+
+        private void CityComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            MyListBox.Items.Clear();
+
+            string selectedType = TypesComboBox.SelectedItem.ToString();
+            string selectedCity = CityComboBox.SelectedItem.ToString();
+            AccommodationService accommodationService = new AccommodationService();
+            foreach (Accommodation entity in accommodationService.GetAll())
+            {
+                if (entity.Location.City.ToString() == selectedCity && entity.Type.ToString() == selectedType)
+                {
+                    MyListBox.Items.Add(entity.Id + " " + entity.Name + " " + entity.Location.City + " " + entity.Location.Country + " " + entity.GuestLimit + " " + entity.MinimalStay + " " + entity.CancelationLimit);
+                }
+
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AccommodationSearchView win2 = new AccommodationSearchView();
+            win2.Show();
         }
     }
 }
