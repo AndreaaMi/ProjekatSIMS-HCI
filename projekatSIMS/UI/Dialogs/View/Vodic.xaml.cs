60b117574272fac05acb5830024b6314f0b22d79
Loha21 <uroslohinski@gmail.com>
2023-03-16 16:06:06 +0100

    textBoxovi

--- projekatSIMS/UI/Dialogs/View/Vodic.xaml.cs
+++ projekatSIMS/UI/Dialogs/View/Vodic.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using projekatSIMS.Model;
+using projekatSIMS.Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,44 @@ namespace projekatSIMS.UI.Dialogs.View
         public Vodic()
         {
             InitializeComponent();
+        }
+
+        private void EnterParamsButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+            TourService tourService = new TourService();
+            Tour newTour = new Tour();
+            newTour.Id = 1;
+            newTour.Name = NameBox.Text;
+            newTour.Location.Country = CountryBox.Text;
+            newTour.Location.City = CityBox.Text;
+            //newTour.Language = ;
+            newTour.StartingDate = DateTime.Parse(DateBox.Text);
+            newTour.StartingTime = TimeBox.Text;
+            newTour.MaxNumberOfGuests = int.Parse(GuestNumBox.Text);
+            newTour.Duration = int.Parse(DurationBox.Text);
+
+            tourService.Add(newTour);
+            foreach(Tour entity in tourService.GetAll())
+            {
+                List1.Items.Add(entity.Id + "|" + entity.Name + "|" + entity.Location.Country + "|" + entity.Location.City + "|" + entity.Language + "|" + entity.StartingDate + "|" + entity.StartingTime + "|" + entity.MaxNumberOfGuests + "|" + entity.Duration);
+            }
+            // Dodajte novi element u ListBox
+            
+
+            // Očistite TextBox-e za sljedeći unos
+            NameBox.Clear();
+            CountryBox.Clear();
+            CityBox.Clear();
+            LanguageBox.Clear();
+            DateBox.Clear();
+            TimeBox.Clear();
+            GuestNumBox.Clear();
+            DurationBox.Clear();
+            DescriptionBox.Clear();
+            KeyPointsBox.Clear();
+
         }
     }
 }
