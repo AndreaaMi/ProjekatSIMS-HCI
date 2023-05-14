c78eaece3eef92386d3f65456e880887eea0d884
Aleksa Vučković <aleksa.vuckovic02@gmail.com>
2023-05-14 21:17:20 +0200

    e

---
+++ projekatSIMS/Repository/IVoucherRepository.cs
@@ -0,0 +1,24 @@
+﻿using projekatSIMS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekatSIMS.Repository
+{
+    public interface IVoucherRepository
+    {
+        void Add(Entity voucher);
+
+        void Remove(Entity voucher);
+
+        Entity Get(int id);
+
+        IEnumerable<Entity> GetAll();
+
+        int GenerateId();
+
+        void Save();
+    }
+}
